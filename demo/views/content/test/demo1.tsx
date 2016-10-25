f73c49d3766adfc0868895708e5d995441c8cd95
ahyiru <ah.yiru@gmail.com>
2016-10-25 11:21:21 +0800

    fix cards style

--- demo/views/content/test/demo1.tsx
+++ demo/views/content/test/demo1.tsx
@@ -1,53 +1,11 @@
 import * as React from 'react';
 
+import {cards} from './data';
+
 import './test.less';
 
-let cards:any=[[{
-  name:'企业信息查询',
-  icon:'fa fa-search',
-  pic:'bg1'
-},
-{
-  name:'企业信用目录',
-  icon:'fa fa-edit',
-  pic:'bg2'
-},
-{
-  name:'异议情况核实',
-  icon:'fa fa-search-plus',
-  pic:'bg3'
-}],[{
-  name:'前置机配置',
-  icon:'fa fa-wrench',
-  pic:'bg4'
-},
-{
-  name:'信息共享管理',
-  icon:'fa fa-commenting',
-  pic:'bg5'
-},
-{
-  name:'统计分析',
-  icon:'fa fa-bar-chart',
-  pic:'bg6'
-}],[{
-  name:'数据归集管理',
-  icon:'fa fa-file',
-  pic:'bg7'
-},
-{
-  name:'数据上报管理',
-  icon:'fa fa-upload',
-  pic:'bg8'
-},
-{
-  name:'支撑管理',
-  icon:'fa fa-user-plus',
-  pic:'bg9'
-}]];
-
 export default class Demo1 extends React.Component<any,any> {
-
+  // cards:any[];
   constructor(props){
     super(props);
     cards.map((v,k)=>{
@@ -84,6 +42,9 @@ export default class Demo1 extends React.Component<any,any> {
       cards:cards
     })
   };
+  gotoPage=()=>{
+    console.log('gotoPage');
+  };
 
   render() {
     var that=this;
@@ -97,7 +58,7 @@ export default class Demo1 extends React.Component<any,any> {
               v.map((sv,sk)=>{
                 return (
                   <div key={`c-${k}-${sk}`} className="y-card">
-                    <article className={sv.pic+' '+sv.animate} onMouseEnter={that.mouseEnter.bind(that,k,sk)} onMouseLeave={that.mouseLeave}>
+                    <a className={sv.pic+' '+sv.animate} onMouseEnter={that.mouseEnter.bind(that,k,sk)} onMouseLeave={that.mouseLeave} onClick={that.gotoPage} href={sv.url}>
                       <div className="b-txt">
                         <div className="txt">{sv.name}</div>
                       </div>
@@ -107,7 +68,7 @@ export default class Demo1 extends React.Component<any,any> {
                       <div className="ico">
                         <div><i className={sv.icon}></i></div>
                       </div>
-                    </article>
+                    </a>
                   </div>
                 )
               })
